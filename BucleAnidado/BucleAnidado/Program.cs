79e275689ca1e1205386d964d30a864a9bf6cfeb
jeyni02 <jeynilomary@uamv.edu.ni>
2024-09-05 19:04:11 -0600

    Add files via upload

---
+++ BucleAnidado/BucleAnidado/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+namespace BucleAnidado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int sum = 0;
+            for (int number = 1; number < 21; number++)
+            {
+                if (number % 3 == 0)
+                {
+                    sum = sum + number;
+                }
+            }
+            Console.WriteLine($"The sum is {sum}");
+        }
+    }
+}
+
