5ab8aa26bc3a480857c5eb2d728b6d53b08908fd
jeyni02 <jeynilomary@uamv.edu.ni>
2024-09-05 19:03:42 -0600

    Add files via upload

---
+++ Bucle/Bucle/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Diagnostics.Metrics;
+
+namespace Bucle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        int counter = 0;
+        for (int counter = 0; counter < 10; counter++)
+       {
+            Console.WriteLine($"Hello World! The counter is {counter}");
+       }
+    }
+}
